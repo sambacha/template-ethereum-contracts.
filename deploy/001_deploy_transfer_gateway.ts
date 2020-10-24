4e573669bcb3853f9fa7352e90df2564ffeab6f9
sam bacha <sam@freighttrust.com>
2020-10-24 13:57:43 -0700

    Initial commit

---
+++ deploy/001_deploy_transfer_gateway.ts
@@ -0,0 +1,16 @@
+import {HardhatRuntimeEnvironment, DeployFunction} from 'hardhat/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+
+  await deploy('ERC20TransferGateway', {
+    from: deployer,
+    log: true,
+    deterministicDeployment: true,
+  });
+};
+export default func;
+func.tags = ['ERC20TransferGateway'];
